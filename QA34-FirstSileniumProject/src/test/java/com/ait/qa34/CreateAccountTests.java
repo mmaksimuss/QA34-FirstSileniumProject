18b3b5dff1647c347929c4fce4e0ca6401f90aae
mmaksimuss <mmaksimus1977@gmail.com>
2024-04-05 11:44:49 +0200

    update createNewAccountPositiveTest

--- QA34-FirstSileniumProject/src/test/java/com/ait/qa34/CreateAccountTests.java
+++ QA34-FirstSileniumProject/src/test/java/com/ait/qa34/CreateAccountTests.java
@@ -13,15 +13,10 @@ public class CreateAccountTests extends TestBase {
         Random random = new Random();
         int i = random.nextInt(1000) + 1000;
         click(By.cssSelector("[href='/register']"));
-        click(By.name("FirstName"));
         type(By.name("FirstName"), "Petr");
-        click(By.name("LastName"));
         type(By.name("LastName"), "Petrov");
-        click(By.name("Email"));
         type(By.name("Email"), "mak28" + i + "@gm.com");
-        click(By.name("Password"));
         type(By.name("Password"), "1234567890");
-        click(By.name("ConfirmPassword"));
         type(By.name("ConfirmPassword"), "1234567890");
         click(By.name("register-button"));
         Assert.assertTrue(isElementPresent(By.cssSelector("[href='/customer/info']")));
